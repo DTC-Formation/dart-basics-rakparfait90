c02ab1dea2fc1c6d9374084d4c0b598b03a8fc90
Rakotoarisaona Parfait <rak.parfait@outlook.com>
2023-09-18 12:05:25 +0300

    ajout fonction consVoyel

--- fonction.dart
+++ fonction.dart
@@ -1,18 +1,19 @@
 void main() {
-  int test = 4;
-  int somme = addition(90, test);
-  print(somme);
+  int a = 4;
+  int b = 90;
+  int somme = addition(a, b);
+  print('La somme de a = $a et b = $b est $somme');
 
   int n = 4;
   int result1 = factoriel(n);
-  print(result1);
+  print('Le factoriel de $n est $result1.');
 
   int base = 2;
   int exposant = 3;
   int result2 = puissance(base, exposant);
-  print(result2);
+  print('$base à la puissance $exposant est $result2');
 
-  String lettre = 'A';
+  String lettre = 'P';
   String result3 = consVoyel(lettre);
   print('$lettre est une $result3.');
 }
@@ -22,12 +23,23 @@ int addition(int nb1, int nb2) {
   return somme;
 }
 
+// int factoriel(int n) {
+//   if (n == 0) {
+//     return 1;
+//   } else {
+//     return n * factoriel(n - 1);
+//   }
+// }
+
 int factoriel(int n) {
-  if (n == 0) {
-    return 1;
-  } else {
-    return n * factoriel(n - 1);
+  int resultat = 1;
+
+  while (n > 0) {
+    resultat *= n;
+    n--;
   }
+
+  return resultat;
 }
 
 int puissance(int nb, int n) {
@@ -46,18 +58,29 @@ String consVoyel(String lettre) {
     'e',
     'i',
     'o',
-    'u'
-  ]; // Ajoutez d'autres voyelles au besoin
+    'u',
+    'à',
+    'â',
+    'é',
+    'è',
+    'ê',
+    'ë',
+    'î',
+    'ï',
+    'ô',
+    'û',
+    'ù',
+    'ü'
+  ];
 
   if (lettre.length == 1) {
-    lettre = lettre
-        .toLowerCase(); // Convertit la lettre en minuscule pour être insensible à la casse
+    lettre = lettre.toLowerCase();
     if (voyelles.contains(lettre)) {
       return 'voyelle';
     } else {
       return 'consonne';
     }
   } else {
-    return 'Veuillez entrer une seule lettre.';
+    return 'deux ou plusieurs lettres';
   }
 }
