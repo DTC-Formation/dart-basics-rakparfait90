2004cdd5d8ab256cbfa7fe7227e72ecedaefb9fd
Rakotoarisaona Parfait <rak.parfait@outlook.com>
2023-09-18 17:15:09 +0300

    class sur dark

---
+++ class.dart
@@ -0,0 +1,35 @@
+class Spacecraft {
+  String name;
+  DateTime? launchDate;
+
+  // Read-only non-final property
+  int? get launchYear => launchDate?.year;
+
+  // Constructor, with syntactic sugar for assignment to members.
+  Spacecraft(this.name, this.launchDate) {
+    // Initialization code goes here.
+  }
+
+  // Named constructor that forwards to the default one.
+  Spacecraft.unlaunched(String name) : this(name, null);
+
+  // Method.
+  void describe() {
+    print('Spacecraft: $name');
+    // Type promotion doesn't work on getters.
+    var launchDate = this.launchDate;
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
+}
+
+void main() {
+  DateTime launchDate = DateTime(1998, 7, 27);
+  Spacecraft spacecraft = Spacecraft("Caprice", launchDate);
+
+  spacecraft.describe();
+}
